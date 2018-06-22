6c4c74db574a6668a19fc33b0b25b3ef27096410
Lainix <linkelin8@gmail.com>
2018-06-21 20:18:53 -0500

    Se agrega html formulario, codigo, tambien se pone boton siguiente y regresar, se pone que solo tome numeros el input telefo de sing

--- js/inicio.js
+++ js/inicio.js
@@ -1,13 +1,3 @@
-function redireccionarlogin() {
-  setTimeout("window.location.href = 'view/login.html", 1);
-  }
-
-function redireccionarsing() {
-  setTimeout("window.location.href = 'view/sing.html", 1);
-  }
-
 // para que lea e inicialice todo
 $(document).ready(function(){
-  // $('.modal').modal();
-  // $('#add-contact').click(getContactData);
 });    
--- js/login.js
+++ js/login.js
@@ -1,33 +1,52 @@
 // Sing
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('select');
   var instances = M.FormSelect.init(elems, options);
+  break;
 });
 
 
 
+// Solo ingrese 10 numeros
+
+
+var raundedNumber = $('.codigo');
+// Generador de numero
+function generator() {
+  var randomNumber = Math.random() * 30;
+  var raundedNumber = Math.ceil(raundedNumber);
+  $("#codigo").HTML = randomNumber;
+}
 
 // login
-
-function getContactData () {
+function getContactData() {
   var $phone = $('#contact-name');
   var $phone = $('#contact-phone');
-  addContacDate($name, $phone.val());
+  // addContacDate($name, $phone.val());
   $('#contact-name').val('');
   $('#contact-phone').val('');
 };
 
-function swalmessage(){
-  // para dar mensaje de listo
+function swalmessage() {
+  // para dar mensaje de success
   swal("Yei!", "datos correctos!", "success");
 }
 
 // para que lea e inicialice todo
-$(document).ready(function() {
+$(document).ready(function () {
   $('select').formSelect();
   $('input#contact-name, input#contact-phone').characterCounter();
   $('#ready').click(swalmessage);
   $('#ready').click(getContactData);
-});
-    
-    
+  $('.form-control').on('input', function () { 
+    this.value = this.value.replace(/[^0-9]/g,'');
+  //   if ($(this).val().length <= 10) {
+  //     $submit = true;
+  //     activeButton();
+  //   } else {
+  //     desactiveButton();
+  // }
+  // });
+      
+  });
+});    
